36dba2d0766068d9bdbe75d35811a4a62e11c179
David Thanphilom <davidthanphilom@msn.com>
2016-01-29 16:55:35 -0800

    added config for $routeProvider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 var app = angular.module("myApp", ['ngRoute']);
+
+app.config(function($routeProvider) {
+        $routeProvider
+
+            .when('/', {
+                templateUrl : 'views/now-playing.html',
+                controller  : 'mainController'
+            })
+
+            .when('/upcoming', {
+                templateUrl : 'views/upcoming.html',
+                controller  : 'upcomingController'
+            })
+
+            .when('/now-showing', {
+                templateUrl : 'views/now-playing.html',
+                controller  : 'mainController'
+            });
+    });
