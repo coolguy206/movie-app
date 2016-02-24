85b547a127eca055c5e02de9c741d5277a135ce2
David Thanphilom <davidthanphilom@msn.com>
2016-02-24 10:09:54 -0800

    cleaned up the js code.

--- js/controllers/detailsController.js
+++ js/controllers/detailsController.js
@@ -1,55 +1,85 @@
-app.controller('detailsController', ['$scope', '$http', '$routeParams', 'nowPlaying', '$sce', function($scope, $http, $routeParams, nowPlaying, $sce) {
+app.controller('detailsController', ['$scope', '$http', '$routeParams', '$sce', function($scope, $http, $routeParams, $sce) {
 
 	var key = "api_key=af8274b3d5d6b03bdbd0443854a35c7a";
 	var apiUrl = 'http://api.themoviedb.org/3/'; 
 
-  nowPlaying.success(function(data){
-    $scope.nowPlaying = data;
-    $scope.whichMovie = $routeParams.movieID;
-    // console.log($scope.whichMovie);
-    // console.log($scope.nowPlaying.results[$scope.whichMovie].id);
+  $scope.whichMovie = $routeParams.movieID;
+  var whichPoster = $routeParams.photoID;
+  // console.log($scope.whichMovie);
+  $scope.posters = {
+    title: 'Posters',
+    show: false,
+    glyphicon: true
+  };
 
-    var movieId = $scope.nowPlaying.results[$scope.whichMovie].id;
+  $scope.cast = {
+    title: 'Cast',
+    show: false,
+    glyphicon: true
+  };
 
-    $http.get(apiUrl + 'movie/' + movieId + '?' + key + '&append_to_response=images,videos,similar,credits&language=en')
-    .success(function(response) { 
-              $scope.details = response;
-              
-              var key = $scope.details.videos.results[0].key;
-              var url = 'https://www.youtube.com/embed/' + key;
-              $scope.youtube = $sce.trustAsResourceUrl(url);
-              
-            }) 
-            .error(function(err) { 
-              return err; 
-            }); 
+  $scope.crew = {
+    title: 'Crew',
+    show: false,
+    glyphicon: true
+  };
 
-  });
-
-
+  $scope.similar = {
+    title: 'Similar Movies',
+    show: false,
+    glyphicon: true
+  };
   
-  $scope.posters = true;
-  $scope.cast = true;
-  $scope.similar = true;
+  $scope.pageClass= 'page-details';
 
-  /*$scope.whichMovie = $routeParams.movieID;
 
-  console.log($scope.nowPlaying.results[$scope.whichMovie].id);
-
-  $scope.getMovieId = function(x) {
-    console.log(x[$scope.whichMovie].id);
-  }*/
-
-  /*$scope.movieId = $scope.nowPlaying.results[$scope.whichMovie].id;
-
-    $http.get(apiUrl + 'movie/' + $scope.movieId + '?' + key + '&append_to_response=images,videos,similar,credits&language=en')
+  $http.get(apiUrl + 'movie/' + $scope.whichMovie + '?' + key + '&append_to_response=images,videos,similar,credits&language=en')
     .success(function(response) { 
-              $scope.details = response;
-              console.log($scope.details);
-            }) 
-            .error(function(err) { 
-              return err; 
-            });*/
+      $scope.details = response;
+      // console.log($scope.details);
+
+      if(angular.equals($scope.details.videos.results[0], undefined)){
+        var key = '';
+      } else {
+        var key = $scope.details.videos.results[0].key;
+      }       
+      var url = 'https://www.youtube.com/embed/' + key;
+      $scope.youtube = $sce.trustAsResourceUrl(url);
+              
+      angular.forEach($scope.details.credits.crew, function(key, val){
+        if(angular.equals(key.profile_path, null)){
+          key.profile_path = 'https://assets.tmdb.org/assets/649ae87ebcf4285d2c45e94407adb382/images/no-profile-w45.jpg'
+          // console.log('its null');
+
+        } else {
+          key.profile_path = 'http://image.tmdb.org/t/p/w185' + key.profile_path;
+        }
+      });
+
+      angular.forEach($scope.details.credits.cast, function(key, val){
+        if(angular.equals(key.profile_path, null)){
+          key.profile_path = 'https://assets.tmdb.org/assets/649ae87ebcf4285d2c45e94407adb382/images/no-profile-w45.jpg'
+          // console.log('its null');
+
+        } else {
+          key.profile_path = 'http://image.tmdb.org/t/p/w185' + key.profile_path;
+        }
+      });
+
+      angular.forEach($scope.details.similar.results, function(key, val){
+        if(angular.equals(key.poster_path, null)){
+          key.poster_path = 'http://ia.media-imdb.com/images/G/01/imdb/images/nopicture/32x44/film-3119741174._CB379391527_.png'
+          // console.log('its null');
+
+        } else {
+          key.poster_path = 'http://image.tmdb.org/t/p/w185' + key.poster_path;
+        }
+      });
+
+    }) 
+    .error(function(err) { 
+      return err; 
+    }); 
 
 }]);
 
--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,12 +1,87 @@
-app.controller('mainController', ['$scope', 'nowPlaying', 'upcoming', function($scope,nowPlaying,upcoming) { 
-  $scope.title = 'Top Sellers in Books'; 
+app.controller('mainController', ['$scope', 'nowPlaying', '$http', function($scope, nowPlaying, $http) { 
+
+	var key = "api_key=af8274b3d5d6b03bdbd0443854a35c7a";
+	var apiUrl = 'http://api.themoviedb.org/3/'; 
 
   nowPlaying.success(function(data){
-  	$scope.nowPlaying = data;
+  	$scope.nowPlaying = data.results;
+    // console.log($scope.nowPlaying);
+
+    angular.forEach($scope.nowPlaying, function(key, val){
+        if (key.overview.length > 150) {
+          var str = key.overview.substring(0,150);
+          var addOn = '... READ MORE';
+          var newStr = str.concat(addOn);
+          key.overview = newStr;
+        }
+      });
+
   });
 
-  upcoming.success(function(obj){
-  	$scope.upcoming = obj;
-  });
+  $scope.pageClass= 'page-home';
+  $scope.myData = true;
+  $scope.views = true;
+
+  $scope.change = function(e){
+  	angular.element('.header-btns div').removeClass('active');
+  	angular.element(e.target).addClass('active');
+    var x = angular.element(e.target).data('name');
+  	// console.log(x);
+    if (x == 'upcoming') {
+      $http.get(apiUrl + 'movie/upcoming?' + key) 
+            .success(function(data) { 
+              // console.log(data);
+              $scope.nowPlaying = data.results;
+
+              angular.forEach($scope.nowPlaying, function(key, val){
+                if (key.overview.length > 150) {
+                  var str = key.overview.substring(0,150);
+                  var addOn = '... READ MORE';
+                  var newStr = str.concat(addOn);
+                  key.overview = newStr;
+                }
+              });
+            }) 
+            .error(function(err) { 
+              return err; 
+            }); 
+    } else if ( x == 'now_playing') {
+      $http.get(apiUrl + 'movie/now_playing?' + key) 
+            .success(function(data) { 
+              // console.log(data);
+              $scope.nowPlaying = data.results;
+
+              angular.forEach($scope.nowPlaying, function(key, val){
+                if (key.overview.length > 150) {
+                  var str = key.overview.substring(0,150);
+                  var addOn = '... READ MORE';
+                  var newStr = str.concat(addOn);
+                  key.overview = newStr;
+                }
+              }); 
+            }) 
+            .error(function(err) { 
+              return err; 
+            }); 
+    } else if ( x == 'popular') {
+      $http.get(apiUrl + 'movie/popular?' + key) 
+            .success(function(data) { 
+              // console.log(data);
+              $scope.nowPlaying = data.results; 
+
+              angular.forEach($scope.nowPlaying, function(key, val){
+                if (key.overview.length > 150) {
+                  var str = key.overview.substring(0,150);
+                  var addOn = '... READ MORE';
+                  var newStr = str.concat(addOn);
+                  key.overview = newStr;
+                }
+              });
+            }) 
+            .error(function(err) { 
+              return err; 
+            }); 
+    }
+  };
 
 }]);
