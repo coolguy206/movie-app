a6436a296960db3ebe77f0a86f662af77f353015
David Thanphilom <davidthanphilom@msn.com>
2016-02-24 10:07:37 -0800

    cleaned up the modules.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,46 @@
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module("myApp", ['ngRoute', 'ngAnimate']);
 
 app.config(function($routeProvider) {
-        $routeProvider
+  $routeProvider
 
-            .when('/', {
-                templateUrl : 'views/now-playing.html',
-                controller  : 'mainController'
-            })
-            .when('/upcoming', {
-                templateUrl : 'views/upcoming.html',
-                controller  : 'mainController'
-            })
-             .when('/movie/:movieID', {
-    			templateUrl: 'views/details.html',
-    			controller: 'detailsController'
-  			});
-            /*.when('/now-showing', {
-                templateUrl : 'views/now-playing.html',
-                controller  : 'js/controllers/mainController'
-            });*/
+    .when('/', {
+        templateUrl : 'views/now-playing.html',
+        controller  : 'mainController'
+    })
+    .when('/search', {
+        templateUrl : 'views/search.html',
+        controller  : 'searchController'
+    })
+    .when('/about', {
+        templateUrl : 'views/about.html',
+        controller  : 'aboutController'
+    })
+    .when('/genre', {
+        templateUrl : 'views/genre.html',
+        controller  : 'genreController'
+    })
+    .when('/genre/:genreID', {
+        templateUrl : 'views/genre-list.html',
+        controller  : 'genreListController'
+    })
+    .when('/movie/:movieID', {
+    		templateUrl: 'views/details.html',
+    		controller: 'detailsController'
+  	})
+  	.when('/movie/:movieID/photo/:photoID', {
+        templateUrl: 'views/photo.html',
+        controller: 'photoController'
+  	})
+  	.when('/person/:personID', {
+        templateUrl: 'views/person.html',
+        controller: 'personController'
+  	})
+  	.when('/person/:personID/photo/:imageID', {
+        templateUrl: 'views/photo.html',
+        controller: 'photoController'
+  	})
+    .otherwise({
+        redirectTo: '/'
     });
+           
+});
